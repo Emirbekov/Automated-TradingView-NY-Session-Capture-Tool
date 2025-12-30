451c8e31498720c27726a85630896d47559f1058
Emir Mirbekov <mirbekov.e.m@gmail.com>
2025-12-30 18:54:38 +0600

    Creates a browser driver only

--- driver.py
+++ driver.py
@@ -1,5 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 def create_driver():
     """
     Creates NON-headless browser
     Loads user profile
     """
+    #Options like maximized
+    options = webdriver.ChromeOptions()
+
+    options.add_argument("--start-maximized")
+
+    options.add_argument("--disable-blink-features=AutomationControlled")
+
+    service = Service(ChromeDriverManager().install())
+
+    driver = webdriver.Chrome(
+        service = service,
+        options = options
+    )
+    
+    return driver
+    
